a54104391665af726643e4c3d70e3e007da3b842
vijaidgr8 <vijai.jayavel@outlook.in>
2024-08-29 12:37:15 +0530

    Script Changes

--- python/basic-examples/lists/merge-strings.py
+++ python/basic-examples/lists/merge-strings.py
@@ -1,15 +1,14 @@
-if _name_ == "_main_":
-    word1 = "a"
-    word2 = "def"
-    result = ""
-    pointer= ""
-    for i in range(min(len(word1),len(word2))):
-        result += word1[i]
-        result += word2[i]
-        pointer = i
-    if len(word1) > len(word2):
-        result += word1[pointer+1:]
-    elif len(word2) > len(word1):
-        result += word2[pointer+1:]
-        
-    print(result)
+word1 = "a"
+word2 = "def"
+result = ""
+pointer= ""
+for i in range(min(len(word1),len(word2))):
+    result += word1[i]
+    result += word2[i]
+    pointer = i
+if len(word1) > len(word2):
+    result += word1[pointer+1:]
+elif len(word2) > len(word1):
+    result += word2[pointer+1:]
+    
+print(result)
