a9e4447c5d2cfcfc37b2102ea470208d893009b8
vijaidgr8 <vijai.jayavel@outlook.in>
2024-08-29 18:26:15 +0530

    Adding Changes for Scripts

--- python/basic-examples/pattern-programs/first-pattern.py
+++ python/basic-examples/pattern-programs/first-pattern.py
@@ -1,4 +1,5 @@
-for i in range(0,5):
+rows = 10
+for i in range(0,rows+1):
     for j in range (0,i+1):
         print("*",end = " ")
     print()
--- python/basic-examples/pattern-programs/pyramid-pattern.py
+++ python/basic-examples/pattern-programs/pyramid-pattern.py
@@ -1,4 +1,4 @@
-rows = 5
+rows = 10
 for i in range (1,rows+1):
     print(" " * (rows-i), end="")
     for j in range (1,i+1):
